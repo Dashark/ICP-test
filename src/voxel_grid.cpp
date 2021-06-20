6c4c1bb509f0d988e5800b2f4015ef773b074b73
Dashark <dasha.hu@gmail.com>
2021-06-20 20:48:33 +0800

    命令行参数指定点云文件

--- src/voxel_grid.cpp
+++ src/voxel_grid.cpp
@@ -12,7 +12,7 @@ main (int argc, char** argv)
   // Fill in the cloud data
   pcl::PCDReader reader;
   // Replace the path below with the path where you saved your file
-  reader.read ("table_scene_lms400.pcd", *cloud); // Remember to download the file first!
+  reader.read (argv[1], *cloud); // Remember to download the file first!
 
   std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height 
        << " data points (" << pcl::getFieldsList (*cloud) << ")." << std::endl;
@@ -20,14 +20,14 @@ main (int argc, char** argv)
   // Create the filtering object
   pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
   sor.setInputCloud (cloud);
-  sor.setLeafSize (atof(argv[1]),atof(argv[2]),atof(argv[3]));
+  sor.setLeafSize (atof(argv[2]),atof(argv[3]),atof(argv[4]));
   sor.filter (*cloud_filtered);
 
   std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height 
        << " data points (" << pcl::getFieldsList (*cloud_filtered) << ")." << std::endl;
 
   pcl::PCDWriter writer;
-  writer.write ("table_scene_lms400_downsampled.pcd", *cloud_filtered, 
+  writer.write (argv[5], *cloud_filtered, 
          Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), false);
 
   return (0);
